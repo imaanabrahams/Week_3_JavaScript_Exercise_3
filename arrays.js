a4c4c3c68fab21c3aae45845fcc6da37de1c2d11
Imaan Abrahams <imaanabrahams15@gmail.com>
2026-05-22 09:34:06 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,23 @@
+// Create an empty array called colors
+let colors = []
+
+// Add three colors to the array using push()
+colors.push("Red");
+colors.push("Pink");
+colors.push("Green");
+
+// Create another array called numbers with five numbers 
+let numbers = [1, 2, 3, 4, 5];
+
+// Remove the last colors from the colors array
+colors.pop();
+
+// Add new color to the beginning of the colors array
+colors.unshift("Mint Green");
+
+// Print the length of both arrays
+console.log("Colors array:", colors);
+console.log("Length of colors array:", colors.length);
+
+console.log("Numbers array:", numbers);
+console.log("Length of numbers array:", numbers.length); 
